79d51683622cf76ada23894b6c1e00e235ac46a5
Jason Manning <jasonkylemanning@gmail.com>
2024-04-03 11:39:17 -0400

    server.py update

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 @app.route('/weather')
 def get_weather():
